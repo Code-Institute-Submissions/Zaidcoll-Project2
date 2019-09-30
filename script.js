bfedf0c1d69bd30bdbea9bac954d64c963b22795
Ubuntu <ubuntu@ip-172-31-35-132.ec2.internal>
2019-09-30 10:07:26 +0000

    Added searchbar and button with API Key

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+/*global $,axios */
 
+const KEY ='f538f37fb7683c00f076bc35ebfcaade'
+const URL ='https://api.themoviedb.org/3/movie/'
+
+function test(){
+    axios.get(URL + '550?api_key=' + KEY).then(function(response){
+    console.log(response)
+    })
+}
+
+test()
 $(function(){
   
 })
