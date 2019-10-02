f3a989c33cdda0f5c6116891713173820902b0c5
Ubuntu <ubuntu@ip-172-31-35-132.ec2.internal>
2019-10-02 06:35:21 +0000

    Added event sequence with search box and button

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 /*global $,axios */
 
 const KEY ='f538f37fb7683c00f076bc35ebfcaade'
-const URL ='https://api.themoviedb.org/3/movie/'
+const URL ='https://api.themoviedb.org/3/search/movie?api_key=' + KEY
+const IMG ='https://image.tmdb.org/t/p/w500'
 
 function test(){
-    axios.get(URL + '550?api_key=' + KEY).then(function(response){
+    axios.get(URL,{
+        params:{
+            'query':searcher,
+            'primary_release_year':'2018',
+            'sort_by':'',
+            'with_genres':''
+        }
+    }).then(function(response){
     console.log(response)
     })
 }
-
-test()
 $(function(){
-  
+    $('#clicker').click(function(){
+        let searcher = $('#searcher').val()
+        axios.get(URL,{
+        params:{
+            'query':searcher,
+            'primary_release_year':'',
+            'sort_by':'',
+            'with_genres':''
+        }
+    }).then(function(response){
+    let movies=response.data.results
+    console.log(response.data)
+    $('#imager1').attr('src',IMG + movies[0].poster_path)
+    })
+    })
+    
+    
+    
 })
+
