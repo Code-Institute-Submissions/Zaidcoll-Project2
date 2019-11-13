a9c07829f6ce7d8df5af9d098db50bbc16e2fa52
Ubuntu <ubuntu@ip-172-31-35-132.ec2.internal>
2019-11-13 11:04:44 +0000

    Added Button with link to more information about the movie

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
 /*global $,axios */
 
-const KEY ='f538f37fb7683c00f076bc35ebfcaade'
-const URL ='https://api.themoviedb.org/3/search/movie?api_key=' + KEY
-const IMG ='https://image.tmdb.org/t/p/w500'
 
-function test(){
-    axios.get(URL,{
-        params:{
-            'query':searcher,
-            'primary_release_year':'2018',
-            'sort_by':'',
-            'with_genres':''
-        }
-    }).then(function(response){
-    console.log(response)
-    })
-}
-$(function(){
-    $('#clicker').click(function(){
+const FIND = 'https://api.themoviedb.org/3/movie/'
+const API_KEY = 'f538f37fb7683c00f076bc35ebfcaade'
+const URL = 'https://api.themoviedb.org/3/search/movie?api_key=' +API_KEY
+const IMG = 'https://image.tmdb.org/t/p/w500'
+const SEARCH = 'https://api.themoviedb.org/3/search/multi?api_key=' + API_KEY
+
+$(function() {
+
+    $('#clicker').click(function() {
+        $('#results').empty()
         let searcher = $('#searcher').val()
-        axios.get(URL,{
-        params:{
-            'query':searcher,
-            'primary_release_year':'',
-            'sort_by':'',
-            'with_genres':''
-        }
-    }).then(function(response){
-    let movies=response.data.results
-    console.log(response.data)
-    $('#imager1').attr('src',IMG + movies[0].poster_path)
+        axios.get(SEARCH, {
+            params: {
+                'query': searcher,
+                'primary_release_year': '',
+                'sort_by': '',
+                'with_genres': ''
+            }
+        }).then(function(response) {
+            let movies = response.data.results
+            let output = '';
+            $.each(movies,(index,movie)=>{
+                output+=`
+                <div class = 'col-md-3'>
+                    <div class="well text-center">
+                    <img src = "${IMG + movie.poster_path}">
+                    <h5>${movie.original_title}</h5>
+                    <h3>${movie.id}</h3>
+                    <a onclick="movieSelected('${movie.id}')" class = "btn btn-primary" href='#'>Movie Details</a>
+                    </div>
+                </div>
+                `
+            })
+            $('#results').html(output);
+            console.log(movies)
+
+        })
     })
-    })
-    
-    
-    
+
 })
 
+function movieSelected(id){
+    sessionStorage.setItem('movieId',id)
+    window.location = 'movieinfo.html';
+    return false;
+}
+
+function movieinfo(){
+    let movieId = sessionStorage.getItem('movieId');
+    
+    axios.get(FIND + movieId +'?api_key='+ API_KEY )
+    .then((response)=>{
+        console.log(response);
+        let movie = response.data;
+        let output = `
+        <div class = "row">
+            <div class = "col-md-4>">
+                <img src = "${IMG + movie.poster_path}" class = "thumbnail">
+            </div>
+        </div>
+        `
+        $('#movie_info').html(output);})
+        
+    }
+
+
+//https://api.themoviedb.org/3/movie/343611?api_key={api_key}
+//Get the movie details with the '343611' as the movie id
+
+//https://api.themoviedb.org/3/movie/157336?api_key={api_key}&append_to_response=videos,images
+//Search for the videos and images using 'append_to_response'
+
+//https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&language=en-US&page=1
+//Search for similar movies using 'similar'
+
+//params : sort_by=vote_average.desc / revenue.desc 
+//genres : https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
